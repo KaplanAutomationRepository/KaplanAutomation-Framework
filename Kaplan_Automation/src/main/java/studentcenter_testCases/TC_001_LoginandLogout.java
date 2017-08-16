f406ee3f617fb3411befbc26cf644c91a3ce1906
KaplanAutomationRepository <divya.suravarjula@kaplan.co.uk>
2017-08-16 16:17:00 +0100

    fjksfsk

--- Kaplan_Automation/src/main/java/studentcenter_testCases/TC_001_LoginandLogout.java
+++ Kaplan_Automation/src/main/java/studentcenter_testCases/TC_001_LoginandLogout.java
@@ -9,27 +9,20 @@ import com.relevantcodes.extentreports.ExtentTest;
 import studentcenter_pages.Login_Page;
 import wrappers.KaplanSpecificWrappers;
 
-public class TC_001_LoginandLogout extends KaplanSpecificWrappers{
+public class TC_001_LoginandLogout extends KaplanSpecificWrappers {
 
-	
 	@BeforeClass
 	public void setData() {
-		testCaseName="TC_001_LoginandLogout";
-		testDescription="Login To MyKaplan and Logout";
-		browserName="chrome";
-		dataSheetName="TC_001_LoginandLogout";
-		category="Smoke";
-		authors="DivyaLakshmi";
+		testCaseName = "TC_001_LoginandLogout";
+		testDescription = "Login To MyKaplan and Logout";
+		browserName = "chrome";
+		dataSheetName = "TC_001_LoginandLogout";
+		category = "Smoke";
+		authors = "DivyaLakshmi";
 	}
-@Test(dataProvider="fetchData")
-public void loginAndLogout(String userName, String password)
-{
-new Login_Page(driver,test)
-.enterUserName(userName)
-.enterPassword(password)
-.clickLogin()
-.clickSignOut();
-}
-}
 
-
+	@Test(dataProvider = "fetchData")
+	public void loginAndLogout(String userName, String password) {
+		new Login_Page(driver, test).enterUserName(userName).enterPassword(password).clickLogin().clickSignOut();
+	}
+}
